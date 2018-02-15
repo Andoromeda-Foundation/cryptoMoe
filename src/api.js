67e53185b2e4ecd7fdb285084ad46978ff35e8d1
xiaodao <lychees67@gmail.com>
2018-02-15 15:43:49 +0800

    Change ABI

--- src/api.js
+++ src/api.js
@@ -11,19 +11,32 @@ const web3 = web3Provider ? new Web3(web3Provider) : new Web3(new Web3.providers
 // const rpc = "https://mainnet.infura.io/WtTerXFEBdFeRdCPm58K";
 
 
-const KittyContract = web3.eth.contract(kittyAbi).at('0x46478a8e504078252166d224b1d164e9c09301f6');
+const KittyContract = web3.eth.contract(kittyAbi).at('0xb80107de25b619da3dcd7f7614d53b69300fe799');
 window.w = web3;
 window.cc = KittyContract;
 
 web3.eth.defaultAccount = web3.eth.accounts[0];
 
 export const getGift = id => new Promise((resolve, reject) => {
-  KittyContract.ownerOf(id, (err, result) => (err ? reject(err) : resolve(result
-  )));
+  KittyContract.getRose(id, (err, result) => (err ? reject(err) : resolve({
+    birthTime: result[0],
+    buyer: result[1],
+    message: result[2],
+    from: result[3],
+    to: result[4],
+    value: result[5],
+    id,
+  })));
 });
 
-export const buyGift = (value) => new Promise((resolve, reject) => {
-  KittyContract.buy((err, result) => (err ? reject(err) : resolve(result)));
+export const buyGift = (from, to, message, value) => new Promise((resolve, reject) => {
+  KittyContract.buyRose(from, to, message, {
+    value: web3.toWei(Number(value), 'ether'),
+    gas: 220000,
+  },
+  (err, result) => (err ? reject(err) : resolve(result)));
 });
 
-export const getTotal = 12;
+export const getTotal = () => new Promise((resolve, reject) => {
+  KittyContract.totalSupply((err, result) => (err ? reject(err) : resolve(result)));
+});
