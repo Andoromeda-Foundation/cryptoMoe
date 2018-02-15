0f70553e10260fc8cb6572d342f59e2d39476a53
xiaodao <lychees67@gmail.com>
2018-02-16 00:35:16 +0800

    Add FAQ page.

--- src/router/index.js
+++ src/router/index.js
@@ -2,16 +2,32 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import ItemView from '@/views/ItemView';
 import ListView from '@/views/ListView';
+import LoginView from '@/views/LoginView';
+import FaqView from '@/views/FaqView';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Home',
-      component: ListView,
+      	name: 'Home',
+        path: '/',        
+      	component: ListView,
     },
-    { name: 'Item', path: '/item/:id(\\d+)', component: ItemView },
+    {
+        name: 'Login',      
+      	path: '/Login',
+      	component: LoginView,
+    },    
+    {
+        name: 'FAQ',
+      	path: '/FAQ',
+      	component: FaqView,
+    },        
+    { 
+    	name: 'Item',
+    	path: '/item/:id(\\d+)',
+    	component: ItemView 
+    },
   ],
 });
---
+++ src/views/FaqView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+  <ul>
+  <li>基础</li>
+    <ul>
+     <li>什么是 CryptoGame</li>    
+      加密游戏（CryptoGame），或者区块链游戏（Blockchain Game）是指利用区块链技术制作的一类去中心化应用。最早可以追溯到 Satoshi Dice，最早的基于以太坊上的游戏，可以追溯到 The King of the Ether。
+      和许多其他的 Dapp 一样，加密游戏也面临着 Scability 所带来的性能瓶颈。
+      因此现在大部分加密游戏都只将用户的资产保存在区块链上。
+      <li>如何购买</li>
+      <li>如何贩卖</li>
+      <li>如何签名</li>
+    </ul>
+    <li>进阶</li>
+    <ul>
+     <li>什么是以太坊</li>
+     <li>什么是 ERC721 Token</li>
+     <li>有哪些加密游戏？</li>
+    </ul>
+  </ul>
+  </div>
+</template>
