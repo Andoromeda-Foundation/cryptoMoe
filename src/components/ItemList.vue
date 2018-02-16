f5d61493eed4d6ba3cef0ca9e71e9957c3654f60
KJlmfe <kjlmfe@gmail.com>
2018-02-16 22:39:53 +0800

    refactor(ListView): isolate ItemList component from ListView view

---
+++ src/components/ItemList.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="columns is-multiline is-mobile">
+    <router-link v-for="item in items"
+                 v-if="item"
+                 :to="{ name: 'Item', params:{id: item.id}}"
+                 :key=item.id.toString()
+                 class="column
+           is-full-mobile
+           is-two-thirds-tablet
+           is-half-desktop
+           is-one-quarter-widescreen
+           is-one-quarter-fullhd">
+      <template v-if="1 <= item.id && item.id <= 114">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-5by4">
+              <img v-lazy="'./static/assets/heros/'+item.id+'.jpg'">
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="content is-small">
+              <h4>{{item.nickname}} · {{item.name}}</h4>
+              <ul>
+                <li>拥有者：
+                  <router-link :to="{ name: 'User', params:{address: item.owner}}">
+                    {{item.owner.slice(-6).toUpperCase()}}
+                  </router-link>
+                </li>
+                <li>当前价格：{{toDisplayedPrice(item.price)}}</li>
+              </ul>
+              <p>标语：{{item.ad}}</p>
+            </div>
+          </div>
+        </div>
+      </template>
+    </router-link>
+  </div>
+</template>
+
+<script>
+import { toReadablePrice } from '@/util';
+
+export default {
+  name: 'item-lists',
+  props: ['items'],
+
+  computed: {},
+
+  methods: {
+    toDisplayedPrice(priceInWei) {
+      const readable = toReadablePrice(priceInWei);
+      return `${readable.price} ${readable.unit}`;
+    },
+  },
+  watch: {},
+};
+</script>
+<style scoped>
+
+</style>
+
