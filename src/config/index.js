d2ed03e76a84c327f3e2d0ac8d64ac8ca1b0a1b2
KJlmfe <kjlmfe@gmail.com>
2018-02-17 23:36:58 +0800

    feat(config): add contract address in Ethereum

--- src/config/index.js
+++ src/config/index.js
@@ -4,8 +4,9 @@ import cardsData from './cards.json';
 
 export const network = {
   1: {
+    default: true, // It will be used when no metaMask
     name: 'Main Ethereum Network',
-    contract: '',
+    contract: '0xd0792ac0de7ef31197c5f452b21a34389ecc725f',
     rpc: 'https://mainnet.infura.io/lTETGFVyQX99UKQ98BN4',
   },
   2: {
@@ -13,7 +14,6 @@ export const network = {
     contract: '',
   },
   3: {
-    default: true, // It will be used when no metaMask
     name: 'Ropsten Test Network',
     contract: '0xcd10ad61d439bf87e3c29fe6bf5e20ac82bf9114',
     rpc: 'https://ropsten.infura.io/lTETGFVyQX99UKQ98BN4',
