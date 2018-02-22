efd2b3cbd4267c2fa7320c372d5a2d9f46e15a46
KJlmfe <kjlmfe@gmail.com>
2018-02-22 16:11:46 +0800

    feat(ItemView): input price in ETH && update UI

--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -4,7 +4,8 @@
       <div class="columns is-multiline is-mobile">
         <div class="column
            is-full-mobile moe-item-display">
-          <img :src="'http://test.cdn.hackx.org/moe/'+item.id+'.jpg'" class="moe-item-img">
+          <img :src="'http://test.cdn.hackx.org/moe/'+item.id+'.jpg'"
+               class="moe-item-img">
         </div>
         <div class="column
            is-full-mobile">
@@ -33,7 +34,8 @@
               <span class="tag is-light">体重</span>{{item.weight}}</p>
             <p class="item-slogan">
               <span class="tag is-black">{{$t('Slogan')}}</span>{{ad}}</p>
-            <article v-if="item.owner !== me.address" class="message is-warning">
+            <article v-if="item.owner !== me.address"
+                     class="message is-warning">
               <div class="message-body">
                 {{$t('EDIT_SLOGAN_TIP')}}
               </div>
@@ -43,17 +45,45 @@
           <template v-if="item.owner !== me.address">
             <div class="buttons">
               <p>
-                <button class="button is-danger is-outlined" @click="onBuy(1)">{{ $t('BUY_BTN') }}</button>
-                <button class="button is-danger is-outlined" @click="onBuy(1.1)">{{ $t('PREMIUM_BUY_BTN', { rate: '10%' }) }}</button>
-                <button class="button is-danger is-outlined" @click="onBuy(1.3)">{{ $t('PREMIUM_BUY_BTN', { rate: '30%' }) }}</button>
-                <button class="button is-danger is-outlined" @click="onBuy(1.5)">{{ $t('PREMIUM_BUY_BTN', { rate: '50%' }) }}</button>
-                <button class="button is-danger is-outlined" @click="onBuy(2)">{{ $t('PREMIUM_BUY_BTN', { rate: '100%' }) }}</button>
+                <button class="button is-danger is-outlined"
+                        @click="onBuy(1)">{{ $t('BUY_BTN') }}</button>
+                <button class="button is-danger is-outlined"
+                        @click="onBuy(1.1)">{{ $t('PREMIUM_BUY_BTN', { rate: '10%' }) }}</button>
+                <button class="button is-danger is-outlined"
+                        @click="onBuy(1.3)">{{ $t('PREMIUM_BUY_BTN', { rate: '30%' }) }}</button>
+                <button class="button is-danger is-outlined"
+                        @click="onBuy(1.5)">{{ $t('PREMIUM_BUY_BTN', { rate: '50%' }) }}</button>
+                <button class="button is-danger is-outlined"
+                        @click="onBuy(2)">{{ $t('PREMIUM_BUY_BTN', { rate: '100%' }) }}</button>
               </p>
-              <p>
-                <button class="button is-danger is-outlined" @click="showInput()" v-show="!isShowInput">手动输入价格</button>
-                <input class="input customize-input" type="text" placeholder="" v-model="customizedPrice" v-show="isShowInput">
-                <button class="button is-success is-outlined" @click="onCustomizedPriceBuy()" v-show="isShowInput">按输入价格购买</button>
+
+              <p v-show="!isShowInput">
+                <button class="button is-danger is-outlined"
+                        @click="showInput">{{$t('INPUT_PRICE_BTN')}}
+                </button>
               </p>
+
+              <div v-show="isShowInput"
+                   class="field has-addons">
+                <p class="control">
+                  <a class="button is-static">
+                    ETH
+                  </a>
+                </p>
+                <p class="control">
+                  <input class="input "
+                         type="number"
+                         placeholder=""
+                         v-model="customizedPrice"
+                         v-show="isShowInput">
+                </p>
+
+                <p class="control">
+                  <button class="button is-success is-outlined"
+                          @click="onCustomizedPriceBuy">{{$t('BUT_AT_INPUT_PRICE_BTN')}}</button>
+                </p>
+              </div>
+
             </div>
             <article class="message is-danger">
               <div class="message-body">
@@ -62,17 +92,19 @@
             </article>
           </template>
 
-          <button v-if="item.owner === me.address" class="button is-warning" @click="onUpdateAd">{{$t('Edit Slogan')}}</button>
-          <button v-if="item.owner === me.address" class="button is-warning" @click="onUpdatePrice">修改价格</button>
+          <button v-if="item.owner === me.address"
+                  class="button is-warning"
+                  @click="onUpdateAd">{{$t('Edit Slogan')}}</button>
+          <button v-if="item.owner === me.address"
+                  class="button is-warning"
+                  @click="onUpdatePrice">{{$t('CHANGE_PRICE_BTN')}}</button>
         </div>
       </div>
       <div class="columns is-multiline is-mobile">
         <div class="column
            is-full-mobile">
           <div class="message-body">
-            <p>
-              <div v-html="item.introduce"></div>
-            </p>
+            <div v-html="item.introduce"></div>
           </div>
         </div>
         <div class="column
@@ -90,14 +122,15 @@
 <script>
 import { buyItem, setGg, setNextPrice, setPrice } from '@/api';
 import { toReadablePrice } from '@/util';
+import web3 from '@/web3';
 
 export default {
   name: 'item-view',
 
   data: () => ({
-      isShowInput: false,
-      customizedPrice: 0,
-    }),
+    isShowInput: false,
+    customizedPrice: 0,
+  }),
 
   computed: {
     itemId() {
@@ -123,8 +156,9 @@ export default {
   methods: {
     showInput() {
       this.isShowInput = true;
-      this.customizedPrice = this.item.price;
+      this.customizedPrice = web3.fromWei(this.item.price, 'ether');
     },
+
     onBuy(rate) {
       if (this.$store.state.signInError) {
         return this.$router.push({ name: 'Login' });
@@ -140,11 +174,12 @@ export default {
           console.log(e);
         });
     },
+
     onCustomizedPriceBuy() {
       if (this.$store.state.signInError) {
         return this.$router.push({ name: 'Login' });
       }
-      const buyPrice = parseFloat(this.customizedPrice).toFixed(0);
+      const buyPrice = web3.toWei(this.customizedPrice, 'ether');
       buyItem(this.itemId, buyPrice)
         .then(() => {
           alert(this.$t('BUY_SUCCESS_MSG'));
@@ -155,10 +190,12 @@ export default {
           console.log(e);
         });
     },
+
     toDisplayedPrice(priceInWei) {
       const readable = toReadablePrice(priceInWei);
       return `${readable.price} ${readable.unit}`;
     },
+
     async onUpdateAd() {
       const ad = prompt(this.$t('UPDATE_SLOGAN_PROMPT'));
       if (ad !== null) {
@@ -176,22 +213,31 @@ export default {
       }
       return 0;
     },
+
     async onUpdatePrice() {
-      const price = prompt(this.$t('UPDATE_PRICE_PROMPT'));
-      if (price !== null) {
-        if (price.length < 10) {
-          return alert(this.$t('UPDATE_PRICE_ERROR_MSG'));
-        }
-        setPrice(this.itemId, price)
-          .then(() => {
-            this.$store.dispatch('FETCH_ITEM', this.itemId);
-          })
-          .catch((e) => {
-            alert(this.$t('UPDATE_PRICE_FAIL_MSG'));
-            console.log(e);
-          });
+      let priceInEth = prompt(this.$t('UPDATE_PRICE_PROMPT'));
+      if (priceInEth === null) {
+        return;
       }
-      return 0;
+      priceInEth = Number(priceInEth);
+      if (isNaN(priceInEth) || priceInEth <= 0) {
+        return alert(this.$t('UPDATE_PRICE_ERROR_MSG'));
+      }
+      const priceInWei = web3.toWei(priceInEth, 'ether');
+      // double confirm
+      const msg = this.$t('UPDATE_PRICE_CONFRIM', { priceInWei, priceInEth });
+      if (!confirm(msg)) {
+        return;
+      }
+
+      setPrice(this.itemId, priceInWei)
+        .then(() => {
+          alert(this.$t('UPDATE_PRICE_SUCCESS_MSG'));
+        })
+        .catch((e) => {
+          alert(this.$t('UPDATE_PRICE_FAIL_MSG'));
+          console.log(e);
+        });
     },
   },
 };
@@ -206,12 +252,10 @@ export default {
   text-align: center;
 }
 .moe-item-img {
-  height: 800px;
+  border-radius: 5px;
+  width: 90%;
 }
 .tag {
   margin-right: 10px;
-}
-.customize-input {
-  width: 450px;
 }
 </style>
